591806a1cde233f2dcc0e49e43cc470e73940c98
mihir1324 <mihirkayath@gmail.com>
2025-06-17 00:21:00 +0545

    Update script.js

--- script.js
+++ script.js
@@ -1,30 +1,14 @@
 document.getElementById("ribbonCutBtn").addEventListener("click", () => {
-  const sound = new Audio("https://www.soundjay.com/button/sounds/button-4.mp3");
-  sound.play();
-
-  // Ribbon animation
-  const ribbon = document.getElementById("ribbonLine");
-  ribbon.style.width = "0";
-  ribbon.style.transition = "width 1s ease-out";
-
-  // Hide button
+  document.getElementById("ribbonLine").style.width = "0";
   document.getElementById("ribbonCutBtn").style.display = "none";
-
-  // Show cloud message
   setTimeout(() => {
-    document.getElementById("catMessage").classList.remove("hidden");
+    document.getElementById("catCloud").classList.remove("hidden");
     document.getElementById("balloons").classList.remove("hidden");
-  }, 1200);
-
-  // Show love letter
-  setTimeout(() => {
-    document.getElementById("loveLetter").classList.remove("hidden");
-  }, 2500);
-
-  // Show angel pic
-  setTimeout(() => {
-    document.getElementById("angelPic").classList.remove("hidden");
-  }, 5500);
+  }, 1000);
+  setTimeout(() => document.getElementById("loveLetter").classList.remove("hidden"), 3000);
+  setTimeout(() => document.getElementById("angelPic").classList.remove("hidden"), 5000);
+  setTimeout(() => document.getElementById("comic").classList.remove("hidden"), 8000);
 });
+
 
 
